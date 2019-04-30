83baa4aa5ea787f7e766d504be44e611a88c3b37
Eddie Barraco <e.barraco@ubitransport.com>
2019-04-30 10:08:57 +0200

    test: Add PurchaseRequest tests

--- tests/Message/PurchaseRequestTest.php
+++ tests/Message/PurchaseRequestTest.php
@@ -12,25 +12,25 @@ class PurchaseRequestTest extends TestCase
     {
         parent::setUp();
         $this->request = new PurchaseRequest($this->getHttpClient(), $this->getHttpRequest());
-        $this->request->initialize(array(
-            'merchantId'                => '12345678',
-            'transactionId'             => '654321',
-            'amount'                    => '15.24',
-            'currency'                  => 'EUR',
-            'testMode'                  => true,
-            'transactionDate'           => '20090501193530',
-            'certificate'               => '1122334455667788',
-            'orderId'                   => '123',
-            'successUrl'                => 'http://success',
-            'cancelUrl'                 => 'http://cancel',
-            'errorUrl'                  => 'http://error',
-            'refusedUrl'                => 'http://refused',
-            'notifyUrl'                 => 'http://notify',
-        ));
     }
 
-    public function testGetData()
+    public function testGetPurchaseData()
     {
+        $this->request->initialize(array(
+            'merchantId' => '12345678',
+            'transactionId' => '654321',
+            'amount' => '15.24',
+            'currency' => 'EUR',
+            'testMode' => true,
+            'transactionDate' => '20090501193530',
+            'certificate' => '1122334455667788',
+            'orderId' => '123',
+            'successUrl' => 'http://success',
+            'cancelUrl' => 'http://cancel',
+            'errorUrl' => 'http://error',
+            'refusedUrl' => 'http://refused',
+            'notifyUrl' => 'http://notify',
+        ));
         $data = $this->request->getData();
 
         $this->assertSame('1524', $data['vads_amount']);
@@ -49,5 +49,82 @@ class PurchaseRequestTest extends TestCase
         $this->assertSame('http://error', $data['vads_url_error']);
         $this->assertSame('http://refused', $data['vads_url_refused']);
         $this->assertSame('1e44c55b059db1b4d1b704d0bcede1fc273add9b', $data['signature']);
+    }
+
+    public function testGetRegisterPayData()
+    {
+        $this->request->initialize(array(
+            'merchantId' => '12345678',
+            'transactionId' => '654321',
+            'amount' => '15.24',
+            'currency' => 'EUR',
+            'testMode' => true,
+            'transactionDate' => '20090501193530',
+            'certificate' => '1122334455667788',
+            'orderId' => '123',
+            'successUrl' => 'http://success',
+            'cancelUrl' => 'http://cancel',
+            'errorUrl' => 'http://error',
+            'refusedUrl' => 'http://refused',
+            'notifyUrl' => 'http://notify',
+            'createCard' => true,
+        ));
+        $data = $this->request->getData();
+
+        $this->assertSame('1524', $data['vads_amount']);
+        $this->assertSame('12345678', $data['vads_site_id']);
+        $this->assertSame('978', $data['vads_currency']);
+        $this->assertSame('INTERACTIVE', $data['vads_action_mode']);
+        $this->assertSame('TEST', $data['vads_ctx_mode']);
+        $this->assertSame('REGISTER_PAY', $data['vads_page_action']);
+        $this->assertSame('SINGLE', $data['vads_payment_config']);
+        $this->assertSame('V2', $data['vads_version']);
+        $this->assertSame('654321', $data['vads_trans_id']);
+        $this->assertSame('20090501193530', $data['vads_trans_date']);
+        $this->assertSame('123', $data['vads_order_id']);
+        $this->assertSame('http://success', $data['vads_url_success']);
+        $this->assertSame('http://cancel', $data['vads_url_cancel']);
+        $this->assertSame('http://error', $data['vads_url_error']);
+        $this->assertSame('http://refused', $data['vads_url_refused']);
+        $this->assertSame('6d56309c59fffb36246fcd95a2de4f2cae611a19', $data['signature']);
+    }
+
+    public function testGetRegisterPayWithCardReferenceData()
+    {
+        $this->request->initialize(array(
+            'merchantId' => '12345678',
+            'transactionId' => '654321',
+            'amount' => '15.24',
+            'currency' => 'EUR',
+            'testMode' => true,
+            'transactionDate' => '20090501193530',
+            'certificate' => '1122334455667788',
+            'orderId' => '123',
+            'successUrl' => 'http://success',
+            'cancelUrl' => 'http://cancel',
+            'errorUrl' => 'http://error',
+            'refusedUrl' => 'http://refused',
+            'notifyUrl' => 'http://notify',
+            'createCard' => true,
+            'cardReference' => 'asuperidentifier',
+        ));
+        $data = $this->request->getData();
+
+        $this->assertSame('1524', $data['vads_amount']);
+        $this->assertSame('12345678', $data['vads_site_id']);
+        $this->assertSame('978', $data['vads_currency']);
+        $this->assertSame('INTERACTIVE', $data['vads_action_mode']);
+        $this->assertSame('TEST', $data['vads_ctx_mode']);
+        $this->assertSame('PAYMENT', $data['vads_page_action']);
+        $this->assertSame('SINGLE', $data['vads_payment_config']);
+        $this->assertSame('V2', $data['vads_version']);
+        $this->assertSame('654321', $data['vads_trans_id']);
+        $this->assertSame('20090501193530', $data['vads_trans_date']);
+        $this->assertSame('123', $data['vads_order_id']);
+        $this->assertSame('http://success', $data['vads_url_success']);
+        $this->assertSame('http://cancel', $data['vads_url_cancel']);
+        $this->assertSame('http://error', $data['vads_url_error']);
+        $this->assertSame('http://refused', $data['vads_url_refused']);
+        $this->assertSame('177ad677db363818e6fbf9f3bf13b7c977648a77', $data['signature']);
     }
 }
